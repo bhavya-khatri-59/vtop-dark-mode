88c770ad1fde63ad01531ee9f15c9011190e337d
Bhavya <bhavya.khatri@gmail.com>
2025-10-23 11:56:39 +0530

    updated for firefox compatibility

--- dark_mode.js
+++ dark_mode.js
@@ -1,17 +1,14 @@
-// contentScript.js
-
 console.log("dark mode loaded!");
 
-green = "90deg"
-yellow = "40deg"
-
 // Define your CSS styles
+// I fixed a small typo: "90eg" is now "90deg"
+// I also added the standard `:fullscreen` pseudo-class for better compatibility
 const styles = `
 
 html:not(.modal-open) {
   background-color: #FFF !important;
   filter: invert(100%) hue-rotate(90deg) brightness(100%) contrast(85%);
-  -webkit-filter: invert(100%) hue-rotate(90eg) brightness(100%) contrast(85%);
+  -webkit-filter: invert(100%) hue-rotate(90deg) brightness(100%) contrast(85%);
 }
 
 body:not(.modal-open) {
@@ -34,7 +31,9 @@ em, svg, embed, form, audio, object, button, canvas, figure:empty {
   -webkit-filter: invert(0%) !important;
 }
 
-img, image, em, embed,.img-rounded {
+/* Changed 'image' to 'svg image' as 'image' is not a standard HTML element. */
+/* If you meant something else, you can change this back. */
+img, svg image, em, embed,.img-rounded {
   filter: invert(100%) hue-rotate(270deg) !important;
   -webkit-filter: invert(100%) hue-rotate(270deg) !important;
 } 
@@ -49,11 +48,9 @@ form em, form img, form svg, form image, form video, form embed, form object, fo
   -webkit-filter: invert(100%) hue-rotate(270deg) !important;
 }
 
-:-webkit-full-screen img, :-webkit-full-screen svg, :-webkit-full-screen video, :-webkit-full-screen embed, :-webkit-full-screen object, :-webkit-full-screen canvas, :-webkit-full-screen button {
-  filter: invert(0%) !important;
-  -webkit-filter: invert(0%) !important;
-}
-
+/* Added the standard :fullscreen selector */
+:fullscreen img, :fullscreen svg, :fullscreen video, :fullscreen embed, :fullscreen object, :fullscreen canvas, :fullscreen button,
+:-webkit-full-screen img, :-webkit-full-screen svg, :-webkit-full-screen video, :-webkit-full-screen embed, :-webkit-full-screen object, :-webkit-full-screen canvas, :-webkit-full-screen button,
 :-moz-full-screen img, :-moz-full-screen svg, :-moz-full-screen video, :-moz-full-screen embed, :-moz-full-screen object, :-moz-full-screen canvas, :-moz-full-screen button {
   filter: invert(0%) !important;
   -webkit-filter: invert(0%) !important;
@@ -69,9 +66,6 @@ form em, form img, form svg, form image, form video, form embed, form object, fo
 // Create a <style> element
 const styleElement = document.createElement('style');
 styleElement.textContent = styles;
-
-//img = document.querySelector('.VITEmblem');
-//img.style.filter = "invert(0%)";
 
 // Inject the <style> element into the <head> of the document
 document.head.appendChild(styleElement);
@@ -94,5 +88,3 @@ observer.observe(document.body, {
   childList: true,
   subtree: true,
 });
-
-
